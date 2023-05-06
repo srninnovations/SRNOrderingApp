b87d9e9ab0ec6a9874afb8ab82c30157bb4dfc94
FerdousFardin <fardinh39u@gmaile.com>
2023-05-06 21:15:04 +0600

    updated ignore.js

--- utils/Ignore.js
+++ utils/Ignore.js
@@ -4,8 +4,8 @@ export default function Ignore() {
   LogBox.ignoreLogs([
     'We can not support a function callback. See Github Issues for details https://github.com/adobe/react-spectrum/issues/2320',
     '`new NativeEventEmitter()` was called with a non-null argument without the required `addListener` method.',
-    '`new NativeEventEmitter()` was called with a non-null argument without the required `removeListeners` method.',
     'Possible Unhandled Promise Rejection (id: 2) Error: ERR_CONNECT Error: ERR_CONNECT',
+    '`new NativeEventEmitter()` was called with a non-null argument without the required `removeListeners` method.',
   ]);
   return;
 }
